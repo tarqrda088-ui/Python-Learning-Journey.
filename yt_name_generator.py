c657031391c74d8261b7c03c41da57629a47df35
tarqrda088-ui <tarqrda088@gmail.com>
2026-03-10 18:42:57 +0200

    Create yt_name_generator.py

---
+++ yt_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the YouTube Channel Name Generator!")
+topic = input("What is your channel topic? \n")
+niche = input("What is your specialty? \n")
+
+print(f"Your suggested channel name is: {topic} {niche} Hub")
