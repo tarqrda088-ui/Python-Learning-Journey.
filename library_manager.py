927bb7d83e352ec9e984bb0fc8ee138cb9235e40
tarqrda088-ui <tarqrda088@gmail.com>
2026-03-10 18:42:09 +0200

    Create library_manager.py

---
+++ library_manager.py
@@ -0,0 +1,8 @@
+# Professional way to merge two lists
+your_library = ["Think and Grow Rich", "Atomic Habits"]
+wishlist = ["Deep Work", "Clean Code"]
+
+# Using '+' to merge lists into a new one
+final_library = your_library + wishlist
+
+print(f"Total books in your library: {final_library}")
